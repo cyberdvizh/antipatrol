ccf9339ce6ff154bb56c695cbf63d8c4aa1da95d
cyberdvizh <59977907+cyberdvizh@users.noreply.github.com>
2020-01-24 19:08:05 +0300

    upd
    upd

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 # -*- config: utf-8
 import telebot
 import markups as m
-
 bot = telebot.TeleBot("995630658:AAF6W6Ksg6fOhd3Zi20McY2OHOekFpAKk9Y")
 
 @bot.message_handler(commands=['start', 'go'])
@@ -22,6 +21,7 @@ def afterstart(message):
         bot.send_message(chat_id, 'Если решитесь, нажмите /start', reply_markup=m.start_markup)
         bot.register_next_step_handler(message, start_handler)
     else:
+        bot.forward_message(int('1001360951458'), chat_id, text)
         bot.send_message(message.chat.id, 'Принимаю местоположение')
         bot.register_next_step_handler(message, afterstart) #askSource
         bot.send_message(chat_id, "Хотите помочь еще?", reply_markup=m.vote_markup)
