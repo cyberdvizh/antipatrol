66e4118f154acd8c407f62a65a35b724353000d2
cyberdvizh <59977907+cyberdvizh@users.noreply.github.com>
2020-01-24 19:25:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,10 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    # forwardMessage
+    from_chat_id = message.chat.id
+    to_chat_id = 1001360951458
+    bot.forward_message(to_chat_id, from_chat_id, message)
 
 bot.polling()
 
